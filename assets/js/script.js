eb7a3973faf6fcd930fe139aaa44615e09b0d22a
Marina <maryna_kuzmiankova@mail.ru>
2022-12-26 12:18:36 +0300

    fix burger-menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
     const menuCloseItem = document.querySelector('.header__nav-close');
-    const menuLinks = document.querySelectorAll('header__link');    
+    const menuLinks = document.querySelectorAll('.header__link');    
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav_active');
     });
